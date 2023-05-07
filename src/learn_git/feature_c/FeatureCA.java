8ba249a0f38b0c8fe5555fcb65d27463ce225dcc
samipiit <sami@piit.us>
2023-05-07 12:13:24 -0400

    Added db connectivity functionality

---
+++ src/learn_git/feature_c/FeatureCA.java
@@ -0,0 +1,13 @@
+package learn_git.feature_c;
+
+import learn_git.feature_a.FeatureAA;
+
+public class FeatureCA {
+
+    String connection;
+
+    FeatureCA(String connection) {
+        this.connection = connection;
+    }
+
+}
